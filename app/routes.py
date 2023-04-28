e450bf5d52eae479b1dfe40caa637c1166141aeb
TombAnneX <109308405+AKTuminello@users.noreply.github.com>
2023-04-28 06:11:30 -0600

    Add Model

--- app/routes.py
+++ app/routes.py
@@ -1,40 +1,40 @@
 from flask import Blueprint, jsonify    
 
-class Book:
-    def __init__(self, id, title, description):
-        self.id = id
-        self.title = title
-        self.description = description
+# class Book:
+#     def __init__(self, id, title, description):
+#         self.id = id
+#         self.title = title
+#         self.description = description
 
-books = [
-    Book(1, "Lord of the It Depends", "A fantasy novel set Philadelphia"),
-    Book(2, "2023: A Depends Odyssey", "A science fiction novel set in Seattle"),
-    Book(3, "Did the Butler Do It? It Depends", "A mystery novel set in the 1920s"),
-    Book(4, "Do You Love Me? It Depends", "A romance novel set New York City"),
-]
+# books = [
+#     Book(1, "Lord of the It Depends", "A fantasy novel set Philadelphia"),
+#     Book(2, "2023: A Depends Odyssey", "A science fiction novel set in Seattle"),
+#     Book(3, "Did the Butler Do It? It Depends", "A mystery novel set in the 1920s"),
+#     Book(4, "Do You Love Me? It Depends", "A romance novel set New York City"),
+# ]
         
 hello_world_bp= Blueprint('hello_world', __name__)
 books_bp = Blueprint("books", __name__, url_prefix="/books") 
 
 @books_bp.route("", methods=["GET"])
-def handle_books():
-    books_response = []
-    for book in books:
-        books_response.append({
-            "id": book.id,
-            "title": book.title,
-            "description": book.description,
-            })
-    return jsonify(books_response)
-@books_bp.route("/<book_id>", methods=["GET"])
-def handle_book(book_id):
-    book_id = int(book_id)
-    for book in books:
-        if book.id == book_id:
-            return {
-                "id": book.id,
-                "title": book.title,
-                "description": book.description,
+# def handle_books():
+#     books_response = []
+#     for book in books:
+#         books_response.append({
+#             "id": book.id,
+#             "title": book.title,
+#             "description": book.description,
+#             })
+#     return jsonify(books_response)
+# @books_bp.route("/<book_id>", methods=["GET"])
+# def handle_book(book_id):
+#     book_id = int(book_id)
+#     for book in books:
+#         if book.id == book_id:
+#             return {
+#                 "id": book.id,
+#                 "title": book.title,
+#                 "description": book.description,
             }
     
 
