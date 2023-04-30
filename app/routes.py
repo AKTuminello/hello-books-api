4220c8bbf02a93db3478946942dc6d6f89b26150
TombAnneX <109308405+AKTuminello@users.noreply.github.com>
2023-04-30 14:05:00 -0600

    Update/Delete routes added

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,21 @@ from app import db
 from app.models.books import Book
 from flask import Blueprint, jsonify, make_response, request, abort
 
-books_bp = Blueprint("books_bp", __name__, url_prefix="/books") 
+books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
+
+#helper functions
+def validate_book(book_id):
+    try:
+        book_id=int(book_id)
+    except:
+        abort(make_response({"message":f"book {book_id} invalid"}, 400))    
+    book = Book.query.get(book_id)
+
+    if not book:
+        abort(make_response({"message":f"book {book_id} not found"}, 404))
+
+        return book
+#route functions
 
 @books_bp.route("", methods=["GET"])
 def read_all_books():
@@ -36,4 +50,24 @@ def read_one_book(book_id):
         "title": book.title,
         "description": book.description,
     }
+@books_bp.route("/<book_id>", methods=["PUT"])
+def update_book(book_id):
+    book = validate_book(book_id)
 
+    request_body=request.get_json()
+
+    book.title = request_body["title"]
+    book.description = request_body["description"]
+
+    db.session.commit()
+    
+    return make_response(f"Book #{book.id} successfully updated")
+
+@books_bp.route("/<book_id>", methods=["DELETE"])
+def delete_book(book_id):
+    book = validate_book(book_id)
+
+    db.session.delete(book)
+    db.session.commit()
+
+    return make_response(f"Book #{book.id} successfully deleted")
