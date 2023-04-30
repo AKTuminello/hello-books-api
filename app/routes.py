23371c2971e1f325d7cbeecd1e9ea9ad113ad8c2
TombAnneX <109308405+AKTuminello@users.noreply.github.com>
2023-04-30 09:07:51 -0600

    reversed get and post to stay with lesson

--- app/routes.py
+++ app/routes.py
@@ -1,34 +1,45 @@
 from flask import Blueprint, jsonify    
 
 from app import db
-from app.models.books import Book
+from app.models.books import Book, jsonify, make_response, request
 
-books_bp = Blueprint("books", __name__, url_prefix="/books") 
 
-@books_bp.route("", methods=["POST"])
+books_bp = Blueprint("books_bp", __name__, url_prefix="/books") 
+
+@books_bp.route("", methods=["GET", "POST"])
 def handle_books():
-    request_body = request.get_json()
-    if "title" not in request_body or "description" not in request_body:
-        return make_response("Invalid Request", 400)
-    
-    new_book = Book(title=request_body["title"],
-                    description=request_body["description"])
-    db.session.add(new_book)
-    db.session.commit()
-    
-    return f"Book {new_book.title} successfully created", 201
+    if request.method == "GET":
+        books = Book.query.all()
+        books_response = []
+        for book in books:
+            books_response.append({
+                "id": book.id,
+                "title": book.title,
+                "description": book.description,
+                })
+        return jsonify(books_response)
+    elif request.method == "POST":
+        request_body = request.get_json()
+        new_book = Book(title=request_body["title"],
+                        description=request_body["description"])
+        db.session.add(new_book)
+        db.session.commit()
+        
+        return f"Book {new_book.title} successfully created", 201
 
 
-# @books_bp.route("", methods=["GET"])
-# def handle_books():
-#     books_response = []
-#     for book in books:
-#         books_response.append({
-#             "id": book.id,
-#             "title": book.title,
-#             "description": book.description,
-#             })
-#     return jsonify(books_response)
+    elif request.method == "GET":
+        books = Book.query.all()
+        books_response = []
+        for book in books:
+            books_response.append({
+                "id": book.id,
+                "title": book.title,
+                "description": book.description,
+                })
+        return jsonify(books_response)
+    
+
 # @books_bp.route("/<book_id>", methods=["GET"])
 # def handle_book(book_id):
 #     book_id = int(book_id)
