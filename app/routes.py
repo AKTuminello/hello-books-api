420fbb801d5ec1bfb19492cf2426ef817952f38d
TombAnneX <109308405+AKTuminello@users.noreply.github.com>
2023-04-30 08:27:21 -0600

    removed all hardcoded

--- app/routes.py
+++ app/routes.py
@@ -1,31 +1,22 @@
-from flask import Blueprint, jsonify, make_response, request
+from flask import Blueprint, jsonify    
 
 from app import db
 from app.models.books import Book
 
-# class Book:
-#     def __init__(self, id, title, description):
-#         self.id = id
-#         self.title = title
-#         self.description = description
-
-# books = [
-#     Book(1, "Lord of the It Depends", "A fantasy novel set Philadelphia"),
-#     Book(2, "2023: A Depends Odyssey", "A science fiction novel set in Seattle"),
-#     Book(3, "Did the Butler Do It? It Depends", "A mystery novel set in the 1920s"),
-#     Book(4, "Do You Love Me? It Depends", "A romance novel set New York City"),
-# ]
-        
 books_bp = Blueprint("books", __name__, url_prefix="/books") 
 
 @books_bp.route("", methods=["POST"])
 def handle_books():
     request_body = request.get_json()
+    if "title" not in request_body or "description" not in request_body:
+        return make_response("Invalid Request", 400)
+    
     new_book = Book(title=request_body["title"],
                     description=request_body["description"])
     db.session.add(new_book)
     db.session.commit()
-    return make_response(f"Book {new_book.title} successfully created", 201)
+    
+    return f"Book {new_book.title} successfully created", 201
 
 
 # @books_bp.route("", methods=["GET"])
